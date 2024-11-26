d8bc7f135665c0bc89549c7a73d096d2f20098b3
Brice AIELLO <brice.aiello.fr@gmail.com>
2024-11-26 16:35:27 +0100

    Configure several vite entrypoints

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                index: 'index.html',
+                resto: 'restaurant.html',
+            }
+        }
+    },
     server: {
         host: "0.0.0.0",
         port: 5173,
